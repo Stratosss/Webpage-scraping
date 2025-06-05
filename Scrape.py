29f57397109676e86b82b99b687ff03072755159
Stratos <157527268+Stratosss@users.noreply.github.com>
2025-06-05 23:28:28 +0100

    Update Scrape.py

--- Scrape.py
+++ Scrape.py
@@ -9,35 +9,34 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
-categories = ['.name', '.pct', '.year', '.wins', '.losses', '.gf', '.ga']
-
-
+column_titles = ['.name', '.year', '.wins', '.losses', '.pct','.gf', '.ga']
+columns = []
 #string makeover
-def construction_function(param):
-    columns = soup.select(param)
-    column_list = []
-    for i in range(len(columns)):
-        temp_column = columns[i].getText().strip() #gets the text between <td class="name"> and </td> and strips/ removes the spaces before and after word
-        column_list.append(temp_column)
-    return column_list
-    
+def construction_function(columns):
+    col = soup.select(columns)
+    col_list = []
+    for i in range(len(col)):
+        temp_column = col[i].getText().strip() #gets the text between <td class="name"> and </td> and strips/ removes the spaces before and after word
+        col_list.append(temp_column)
+    return col_list
+   
     
 def results(param):
     while True:
             x = input("What do you wish to do? Press 1 for: Print all data or press 2 for: Print the teams that have a win rate >50%? (Quit with q): ")
             if x == "1":
                 df = pd.DataFrame({
-                                    "Team Name" : categories[0], 
-                                    "Year" : categories[1], 
-                                    "Wins" : categories[2],
-                                    "Losses": categories[3], 
-                                    "Win %" : categories[4], 
-                                    "Goals for" : categories[5], 
-                                    "Goals against" :categories[6]})
+                                    "Team Name" : param[0], 
+                                    "Year" : param[1], 
+                                    "Wins" : param[2],
+                                    "Losses": param[3], 
+                                    "Win %" : param[4], 
+                                    "Goals for" : param[5], 
+                                    "Goals against" : param[6]})
                 return df
                 
             elif x == "2":
-                total= np.column_stack([categories[0], categories[1]])      #attempt to combine numpy and pandas
+                total= np.column_stack([param[0], param[4]])      #attempt to combine numpy and pandas
                 headers = ["Team Name", "Win %"]
 
                 rows = total.shape[0]
@@ -57,11 +56,8 @@ def results(param):
                 sys.exit()
             else:
                 print("Please select: 1 or 2 for printouts, or q for exit")
-                
-if __name__=="__main__":                   
-    for i in range(len(categories)):
-        categories[i]= construction_function(categories[i])
-    print(results(categories))
-    
-    
-    
+      
+if __name__=="__main__":
+    for i in range(len(column_titles)):
+        columns.append(construction_function(column_titles[i]))
+    print(results(columns))
